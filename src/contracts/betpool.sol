bc9345ed4156650462cada29d2850923bcea9a83
kevnguyuga@gmail.com <kevnguyuga@gmail.com>
2021-04-14 01:23:32 -0400

    added send winnings function

--- src/contracts/betpool.sol
+++ src/contracts/betpool.sol
@@ -24,4 +24,26 @@ contract Betpool is Calculate {
         _options.push(opt2);
         _countDifference();
     }
+
+    function reset() external onlyOwner {
+        delete _players;
+        delete _options;
+        _choice1Winnings = 0;
+        _choice2Winnings = 0;
+    }
+
+    function sendWinnings(string memory winner) external payable onlyOwner {
+        _countDifference();
+        // (winnings * percent) / 100 will give decimal result but in int
+        uint256 percentage = _divide(winner);
+        uint256 bonus;
+        if (
+            keccak256(abi.encodePacked(winner)) ==
+            keccak256(abi.encodePacked(_options[0]))
+        ) {
+            bonus = (_choice1Winnings * percentage) / 100;
+        } else {
+            bonus = (_choice2Winnings * percentage) / 100;
+        }
+    }
 }
--- src/contracts/calculate.sol
+++ src/contracts/calculate.sol
@@ -4,6 +4,8 @@ import "./storage.sol";
 contract Calculate is Storage {
     mapping(string => uint256) internal _betChoices;
     string[] internal _options;
+    uint256 internal _choice1Winnings;
+    uint256 internal _choice2Winnings;
 
     modifier isOptions(string memory bet) {
         require(
@@ -25,8 +27,10 @@ contract Calculate is Storage {
                 keccak256(abi.encodePacked(_options[0]))
             ) {
                 counter1++;
+                _choice1Winnings += _betAmount[_players[i]];
             } else {
                 counter2++;
+                _choice2Winnings += _betAmount[_players[i]];
             }
         }
         _betChoices[_options[0]] = counter1;
@@ -35,29 +39,24 @@ contract Calculate is Storage {
 
     function _getPercentage() internal view returns (uint256, uint256) {
         uint256 total = _betChoices[_options[0]] + _betChoices[_options[1]];
-        uint256 pcent1 = _betChoices[_options[0]] / total;
-        uint256 pcent2 = _betChoices[_options[1]] / total;
+        uint256 pcent1 = (_betChoices[_options[0]] * 100) / total;
+        uint256 pcent2 = (_betChoices[_options[1]] * 100) / total;
         return (pcent1, pcent2);
     }
 
-    function _divide(string memory loser) internal view returns (uint256) {
+    function _divide(string memory winner) internal view returns (uint256) {
         uint256 p1;
         uint256 p2;
         uint256 percent = 0;
         (p1, p2) = _getPercentage();
         if (
-            keccak256(abi.encodePacked(loser)) ==
+            keccak256(abi.encodePacked(winner)) ==
             keccak256(abi.encodePacked(_options[0]))
         ) {
-            percent = p1 / _betChoices[_options[1]];
-        } else {
             percent = p2 / _betChoices[_options[0]];
+        } else {
+            percent = p1 / _betChoices[_options[1]];
         }
         return percent;
-    }
-
-    function empty() internal {
-        delete _players;
-        delete _options;
     }
 }
